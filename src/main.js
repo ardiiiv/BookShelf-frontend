24da2bf446334bef894b81d7901d51614646ce90
ardiiiv <ardiiiv222@gmail.com>
2025-11-24 13:51:47 +0700

    1. install ohVueIcon dan menambahkan icon. 2. menambahkan gaya khusus di tailwind.config.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { MdPostaddTwotone, MdMenubookTwotone, MdPeoplealtOutlined, MdAccountcircleTwotone } from "oh-vue-icons/icons/md"
+import { RiBookMarkLine } from "oh-vue-icons/icons/ri"
+import { HiHome } from "oh-vue-icons/icons/hi"
 
+addIcons(
+    MdPostaddTwotone,
+     MdMenubookTwotone,
+     RiBookMarkLine,
+     MdPeoplealtOutlined,
+     HiHome,
+     MdAccountcircleTwotone
+)
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component("v-icon", OhVueIcon);
 app.mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Poppins: ["Poppins", "sans-serif"],
+        Playfair: ["Playfair Display", "serif"],
+      },
+      colors: {
+        Primary: "#DA1E37",
+        Secondary: "#C71F37",
+        SlateGrey: "#6C757D"
+      },
+      boxShadow: {
+        PrimaryShadow: "0 15px 40px rgba(154, 160, 185, 0.05), 0 15px 40px rgba(166, 173, 201, 0.2)"
+      }
+    },
   },
   plugins: [],
 }
