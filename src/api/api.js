60ea5773f88258ca75c94e7d55d0c409e63d8439
ardiiiv <ardiiiv222@gmail.com>
2025-12-22 17:30:12 +0700

    revisi api.js dan inputField.vue

--- src/api/api.js
+++ src/api/api.js
@@ -18,6 +18,7 @@ const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api"
 const api = axios.create({
     baseURL: BASE_URL,
     timeout: 10000,
+    withCredentials: true
 })
 
 // === Interceptor untuk Authorization token ===
--- src/components/input/InputField.vue
+++ src/components/input/InputField.vue
@@ -1,14 +1,38 @@
 <script setup>
-const { type = "text", title = "title", required = false } = defineProps({
+const props = defineProps({
+  modelValue: {
     type: String,
-    title: String,
-    required: Boolean
+    default: ""
+  },
+  type: {
+    type: String,
+    default: "text"
+  },
+  title: {
+    type: String,
+    default: "title"
+  },
+  required: {
+    type: Boolean,
+    default: false
+  }
 })
+
+const emit = defineEmits(["update:modelValue"])
 </script>
 
 <template>
-    <div class="flex flex-col gap-2 mb-4">
-        <label class="md:text-lg text-base font-Poppins font-medium text-Primary">{{ title }}</label>
-        <input class=" py-2 px-4 rounded-md ring-1 ring-SlateGrey focus:outline-2 focus:outline-Primary" :type="type" :required="required">
-    </div>
+  <div class="flex flex-col gap-2 mb-4">
+    <label class="md:text-lg text-base font-Poppins font-medium text-Primary">
+      {{ title }}
+    </label>
+
+    <input
+      class="py-2 px-4 rounded-md ring-1 ring-SlateGrey focus:outline-2 focus:outline-Primary"
+      :type="type"
+      :required="required"
+      :value="modelValue"
+      @input="emit('update:modelValue', $event.target.value)"
+    />
+  </div>
 </template>
